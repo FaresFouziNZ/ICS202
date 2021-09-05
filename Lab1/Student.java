dcc982276a629868574e12e2cc8cc7acf4b0c491
FaresFouziNZ <74138397+FaresFouziNZ@users.noreply.github.com>
2021-09-05 19:13:43 +0300

    First lab initial

---
+++ Lab1/Student.java
@@ -0,0 +1,13 @@
+package Lab1;
+
+public abstract class Student {
+    protected int ID;
+    protected double GPA;
+    public abstract String getStatue();
+    public String displayStudent(){
+        return "s";
+    }
+
+    
+
+}
