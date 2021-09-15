6512b9a066dab0dd4beb81aba5534b8e0577e478
FaresFouziNZ <74138397+FaresFouziNZ@users.noreply.github.com>
2021-09-15 11:58:26 +0300

    Lab2 warm-up

---
+++ Lab2/LinkedListApplication.java
@@ -0,0 +1,10 @@
+public class LinkedListApplication {
+	public static void main(String[] args) {
+		SLL<String> myList = new SLL<String>();
+		for(int i = 0; i < 5; i++)
+			myList.addToHead("A" + i);
+			
+		myList.printAll();
+		System.out.print(myList.length());
+	}
+}
